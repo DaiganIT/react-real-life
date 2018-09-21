02dc004eabbfbb64a90b697e53a73646ff2f5662
Pietro Carta <pietro.carta88@gmail.com>
2018-09-21 15:37:38 +0100

    added display state in the first textbox example

--- src/js/examples/textbox/first.js
+++ src/js/examples/textbox/first.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import TextBox from './textbox-sfc';
 import TextBoxCode from './textbox-sfc.txt';
 import Example from '../../components/example';
-import { Typography } from '../../../../node_modules/@material-ui/core';
 
-export default class TextBoxExample extends React.Component {
+class TextBoxExample extends React.Component {
   state = {
     value: '',
   };
@@ -14,18 +16,39 @@ export default class TextBoxExample extends React.Component {
   };
 
   render() {
+    const { classes } = this.props;
+
     return (
       <React.Fragment>
         <Typography variant="title">
           Textbox as a Stateless Functional Component
         </Typography>
         <Typography variant="body1">
-          This is the way I prefer my Text Boxes to be. It is a controlled component and the state is handled in the parent component
+          This is the way I prefer my Text Boxes to be. It is a controlled
+          component and the state is handled in the parent component
         </Typography>
         <Example code={TextBoxCode}>
-          <TextBox value={this.state.value} onChange={this.onChange('value')} />
+          <div className={classes.textBox}>
+            <TextBox
+              value={this.state.value}
+              onChange={this.onChange('value')}
+            />
+          </div>
+          <div>State on parent component value: {this.state.value}</div>
         </Example>
       </React.Fragment>
     );
   }
 }
+
+TextBoxExample.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const styles = (theme) => ({
+  textBox: {
+    marginBottom: theme.spacing.unit,
+  },
+});
+
+export default withStyles(styles)(TextBoxExample);
