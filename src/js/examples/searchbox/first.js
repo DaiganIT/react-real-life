b437af3519cbbee09fbdde3df5e14e484f98c22c
Pietro Carta <pietro.carta88@gmail.com>
2018-09-22 21:30:16 +0100

    added searchbox code

--- src/js/examples/searchbox/first.js
+++ src/js/examples/searchbox/first.js
@@ -4,6 +4,7 @@ import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import SearchBox from './searchbox';
 import SearchContainer from './search-container';
+import SearchBoxCode from './searchbox.txt';
 import Example from '../../components/example';
 
 class SearchBoxExample extends React.Component {
@@ -17,15 +18,16 @@ class SearchBoxExample extends React.Component {
           When it comes to a general search box in the App Bar, this is the
           pattern I use for having the search box interacting with the currently
           active component. The search box will expose a static method so
-          components can listen to events.
+          components can listen to events. This pattern is well suited for
+          global components that will be present only once in the page.
+          I use the same pattern for Login and Global Dialogs.
         </Typography>
-        <Example code={''}>
+        <Example code={SearchBoxCode}>
           <div className={classes.textBox}>
             <SearchBox />
             <SearchContainer />
           </div>
         </Example>
-        
       </React.Fragment>
     );
   }
